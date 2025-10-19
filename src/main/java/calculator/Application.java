c6e54b01085b5cc74baaca34c415e34bc2120ae0
Profile-exe <kang67572346@gmail.com>
2025-10-19 18:05:38 +0900

    feat: 문자열 덧셈 계산기 어플리케이션 로직 작성
    - OutputView를 통한 CLI 문구 출력
    - Console API를 통한 사용자 입력 받기
    - Calculator를 통한 문자열 덧셈 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.io.OutputView;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        outputView.showInputGuide();
+
+        String input = Console.readLine();
+
+        int result = Calculator.calculate(input);
+
+        outputView.showResult(result);
+
+        Console.close();
     }
 }
